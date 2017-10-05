6734dc104d5088bbf9599a5daa66ba75697d7dd4
JeGar9 <segadordracon@gmail.com>
2017-10-05 16:11:24 +0200

    Enemy Description
    The branch to create the enemies for the game

---
+++ Assets/GravityGame/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public float speed = 1.0f;
+    public List<GameObject> trialName;
+
+
+    private int nodeNumber;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
