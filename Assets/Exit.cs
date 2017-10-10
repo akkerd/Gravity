495476668a5765a835375afcaa1934df0450198e
mloppnow <malloppnow@gmail.com>
2017-10-10 12:08:52 +0200

    Level Change
    Switch level when reach portal

--- Assets/Exit.cs
+++ Assets/Exit.cs
@@ -3,22 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Exit : MonoBehaviour {
+public class Exit : MonoBehaviour
+{
 
     private GameObject player;
-
+    public static int currentLevel = 0;
+    public string[] levelNames = new string[] { "Level_1", "Level_2", "Level_3" };
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         player = other.gameObject;
+
+        // check if collider is player 
+        if (player.tag == "Player")
+        {
+            if (currentLevel < levelNames.Length)
+            {
+                currentLevel++;
+                //Scene sceneToLoad = SceneManager.GetSceneByName(levelNames[currentLevel]);
+                SceneManager.LoadScene(levelNames[currentLevel], LoadSceneMode.Single);
+                //Application.LoadLevel(levelNames[currentLevel]);
+
+            }
+        }
+
     }
 }
