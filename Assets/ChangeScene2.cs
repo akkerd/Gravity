c1fa7c19a89c3c852496558b9bd7be8d98e748f9
Diego Tobarra <dtbarra@gmail.com>
2017-11-07 12:08:52 +0100

    Add my part as a separate branch

---
+++ Assets/ChangeScene2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene2 : MonoBehaviour {
+
+    private bool inTrigger;
+	// Use this for initialization
+	void Start () {
+        inTrigger = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( inTrigger)
+        {
+            if (Input.GetButtonDown("Action"))
+            {
+                // Only specifying the sceneName or sceneBuildIndex will load the scene with the Single mode
+                SceneManager.LoadScene("B2-SwitchCam");
+            }
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inTrigger = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inTrigger = false;
+        }
+    }
+}
